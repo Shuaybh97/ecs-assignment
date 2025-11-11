61c90047b0511e3117fe08552614b0df4d0a6140
Shuaib Hussein <93594848+Shuaybh97@users.noreply.github.com>
2025-11-11 23:18:54 +0000

    adding r53 hosted zone

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -15,6 +15,11 @@ resource "aws_route53_record" "domain_name_record" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = aws_route53_zone.primary.name
   type    = "A"
+  alias {
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
+    evaluate_target_health = true
+  }
   ttl     = 60
   records = [aws_lb.alb.dns_name]
 }
