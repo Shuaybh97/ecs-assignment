ef35ca9eb400faadde70a1387f4a73fa1c675b87
Shuaib Hussein <93594848+Shuaybh97@users.noreply.github.com>
2025-11-11 18:22:28 +0000

    fixing workflow

--- terraform/bootstrap/data.tf
+++ terraform/bootstrap/data.tf
@@ -30,7 +30,7 @@ data "aws_iam_policy_document" "github_actions_policy" {
     actions = [
       "iam:GetPolicyVersion",
     ]
-    resources = [aws_iam_policy.github_actions_bootstrap_policy]
+    resources = [aws_iam_policy.github_actions_bootstrap_policy.arn]
   }
 }
 
