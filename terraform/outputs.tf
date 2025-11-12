cabc37016a1f702fb31613d24aba4ec7224ebd70
Shuaib Hussein <93594848+Shuaybh97@users.noreply.github.com>
2025-11-12 00:32:50 +0000

    adding ci workflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,3 +14,17 @@ output "ecr_repository_arn" {
 }
 
 
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.this.name
+}
+
+output "ecs_service_name" {
+  description = "The name of the ECS service"
+  value       = aws_ecs_service.fargate_service.name
+}
+
+output "task_definition_arn" {
+  description = "The ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.app.arn
+}
